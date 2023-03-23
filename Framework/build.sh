3ebcd9dfa5cc48c201fc046beb7d8196f32f7a9a
Aris <aris@pop-os.localdomain>
2023-03-23 05:32:56 +0300

    Ajout Build.sh

---
+++ Framework/build.sh
@@ -0,0 +1,4 @@
+cd ./build/web/WEB-INF 
+jar cf ./framework.jar ./classes/etu2083
+mkdir /home/aris/S4-ITU/Framework-ETU2083/Test-Framework/web/WEB-INF/lib
+cp ./framework.jar /home/aris/S4-ITU/Framework-ETU2083/Test-Framework/web/WEB-INF/lib
